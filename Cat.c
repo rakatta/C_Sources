f9fadffe1f1c59f733e3c2b2701e9d9fe61ebc5e
rakatta <64879179+rakatta@users.noreply.github.com>
2020-11-14 19:57:38 -0600

    Update Cat.c

--- Cat.c
+++ Cat.c
@@ -5,7 +5,7 @@
 
 
 #include<stdio.h>
-#include<LinuxConio.h>
+#include"LinuxConio.h"
 #include<string.h>
 #include<stdlib.h>
 
@@ -13,29 +13,37 @@ int main (int argc, char *argv[])
 
 {
   clrscr();
-  
+  char ch;
   if (argc>=2)
   {
    FILE* file1 = fopen(argv[1], "r");
-   char ch;
-   if (file1 != NULL )
-   {
-    while ( (ch=fgetc(file1) ) != EOF  )
+   if ( file1 == NULL )
+   printf("File is not present\n");
+   else{
+    
+    if (file1 != NULL )
     {
-     printf("%c",ch);
+     while ( (ch=fgetc(file1) ) != EOF  )
+     {
+      printf("%c",ch);
+     }
     }
-   }
+    
    fclose(file1); 
   }
+  return 0;
+ }
   else
    puts("Enter any file at the commandline");
-   
    
   // take an input at command for the file path and serve the file to the pointer to read contents.
   char s[150];
   gets(s); 
   FILE* file2 = fopen(s, "r");
-   char ch;
+  if ( file2 == NULL )
+  printf("File is not present\n");
+  else{
+   
    if (file2 != NULL )
    {
     while ( (ch=fgetc(file2) ) != EOF  )
@@ -43,7 +51,9 @@ int main (int argc, char *argv[])
      printf("%c",ch);
     }
    }
+   
    fclose(file2); 
+  }
   return 0;
  }
    
